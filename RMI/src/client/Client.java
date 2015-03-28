bd70f828a54878d6368c5db119013cdb05901f36
Jérémy Fontaine <jeremy.fontaine@live.fr>
2015-03-28 23:03:06 +0100

    Mise à jour client

--- RMI/src/client/Client.java
+++ RMI/src/client/Client.java
@@ -1,12 +1,31 @@
 package client;
 
-public class Client
+import interf.SiteItf;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import message.Message;
+
+public  class   Client
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws RemoteException, NotBoundException
 	{
-		String hote = args[0];
-		int    port = Integer.parseInt(args[1]);
+		String  hote    = args[0];
+		int     port    = Integer.parseInt(args[1]);
+		String  message = "toto";
 		
+		if(args.length > 2)
+			message = args[2];
 		
+		Registry reg  = LocateRegistry.getRegistry(hote, port);
+		SiteItf  site = (SiteItf) reg.lookup("RMI");
+		int      flag = site.genereFlag();
+	
+		Message  msg  = new Message(message, flag);
+		
+		site.transfert(msg);
 	}
 }
